14fdaee26e44fb869e34ca948ec50a3af6ab0738
Venkat <venkat.baladhandayutham@toshibauk.onmicrosoft.com>
2021-06-22 16:43:44 +0100

    Setting up the repo with base code

---
+++ src/test/java/com/daytodayhealth/rating/RatingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daytodayhealth.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
